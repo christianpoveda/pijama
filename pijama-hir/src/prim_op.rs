89dab8fdf2312a2c5f49c919c55ba8fcabc6677b
Christian Poveda <git@christianpoveda.xyz>
2020-12-11 22:21:38 +0100

    add primitive operators

--- pijama-hir/src/prim_op.rs
+++ pijama-hir/src/prim_op.rs
@@ -1,3 +1,34 @@
 /// A primitive operator.
 #[derive(Debug)]
-pub enum PrimOp {}
+pub enum PrimOp {
+    /// The logical negation operator.
+    Not,
+    /// The arithmetic negation operator.
+    Neg,
+    /// The arithmetic addition operator.
+    Add,
+    /// The arithmetic substraction operator.
+    Sub,
+    /// The arithmetic multiplication operator.
+    Mul,
+    /// The arithmetic division operator.
+    Div,
+    /// The arithmetic remainder operator.
+    Rem,
+    /// The logical conjunction operator.
+    And,
+    /// The logical disjunction operator.
+    Or,
+    /// The equality operator.
+    Eq,
+    /// The "not equal to" operator.
+    Neq,
+    /// The "less than" operator.
+    Lt,
+    /// The "greater than" operator.
+    Gt,
+    /// The "less than or equal to" operator.
+    Lte,
+    /// The "greater than or equal to" operator.
+    Gte,
+}
