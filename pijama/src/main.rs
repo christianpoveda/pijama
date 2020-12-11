5d3fb1050d6b96c515f8a2dc42e1dc31f08b1294
Christian Poveda <git@christianpoveda.xyz>
2020-12-11 14:19:14 +0100

    add executable

--- pijama/src/main.rs
+++ pijama/src/main.rs
@@ -1,3 +1,16 @@
+use std::{env::args, fs::read_to_string};
+
 fn main() {
-    println!("Hello, world!");
+    // Get the path of the file with the source code.
+    let mut args = args();
+    args.next().unwrap();
+    let path = args.next().unwrap();
+
+    // Read the source code to a string.
+    let source = read_to_string(path).unwrap();
+
+    // Parse the source code.
+    let ast = pijama_parser::parse(&source);
+
+    println!("{:?}", ast);
 }
