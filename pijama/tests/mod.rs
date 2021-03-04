a37f4c74585baa55984771071c992673a107e29a
Christian Poveda <git@christianpoveda.xyz>
2021-03-03 21:32:25 -0500

    add test suite

---
+++ pijama/tests/mod.rs
@@ -0,0 +1,16 @@
+macro_rules! check {
+    ($name:ident) => {
+        #[test]
+        fn $name() {
+            let config = pijama_interface::Config {
+                path: concat!("tests/", stringify!($name), ".pj").into(),
+                codegen: false,
+            };
+
+            pijama_interface::Compiler::new().run(config);
+        }
+    };
+}
+
+check!(fibonacci);
+check!(apply);
